cdcd7b5a0a6f5c807fa07b1baa6c905940220dce
tmz000 <465284105@qq.com>
2021-04-20 10:20:24 +0800

    代码优化

--- src/main/resources/static/getMsUser.js
+++ src/main/resources/static/getMsUser.js
@@ -39,47 +39,6 @@ $('a').click(function(){
 	}
 })
 
-/*$("#price").change(function(){
-	var price = $(this).val();
-	$("#price").val((price/1).toFixed(2));
-})*/
-
-/*$("#discount").keyup(function(){
-	var num = 0;
-	var freeMoney=0;
-	var count = $("#count").val();
-	var discount = $("#discount").val();
-	var price = $("#price").val();
-	if(discount/1 > 10){
-		$("#discount").val("")
-		alert("请输入1-10之间的折扣数！")
-	}else{
-		if(price != ""){
-			freeMoney = (price/1-price*discount*0.1).toFixed(2);
-			if(count != "" && count != undefined){
-				freeMoney = (freeMoney*count/1).toFixed(2);
-				$("#freeMoney").val(freeMoney); 
-				num = (price*discount*count*0.1).toFixed(2);
-			}else{
-				$("#freeMoney").val(freeMoney); 
-				num = (price*discount/10).toFixed(2);
-			}
-		}else{
-			$("#discount").val("")
-			alert("产品和价格不能为空！")
-			//alert("请先输入折扣数（0-10直接的整数或者小数）！")
-		}
-		if(num >= 0){
-			$("#amount").val(num)
-			$("#trueMoney").val(num)
-		}else{
-			$("#amount").val("0")
-			$("#trueMoney").val("0")
-			alert("折扣数过低！")
-		}
-	}
-})*/
-
 String.prototype.startWith=function(str){  
     if(str==null||str==""||this.length==0||str.length>this.length)  
       return false;  
